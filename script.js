10ae280b7ba1ed7c5d84e0a35c8c3a61679cb9cc
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-15 23:27:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 function firstWord(s) {
-  // your code here
-	let first =""; 
-	for(let i=0; i<s.length; i++){
-    
-		if(s[i]==' '){
-         return first;			
-
-			
-		}
-		 first += s[i];
-	}
-	return first;
+  s = s.trim() + " ";
+  let firstBlank = s.indexOf(" ");
+  return s.substr(0, firstBlank);
 }
-
 // Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
