dfd2ad01945977c456f5cd4e44da04da56c7c7ef
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-15 23:08:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	let first = ""; 
+	for(let i=0; i<s.length; i++){
+     
+		if(s[i]==' '){
+         return first;
+			
+		}
+		 first += s[i];
+	}
+	return first;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
